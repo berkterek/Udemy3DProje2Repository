cd12df6226aaec2901b233a27196e95cf5f7e15d
berkterek <42406263+berkterek@users.noreply.github.com>
2021-02-22 16:09:24 +0300

    EnemyScript and Spanwers
    Enemy Script ve enemy'inin kendisi + varialt'i olusturldu ve spanwer'lar gameobject olarak olustu ama daha script yazilmadi

---
+++ UdemyProject2/Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UdemyProject2.Movements;
+using UnityEngine;
+
+namespace UdemyProject2.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] float _moveSpeed = 10f;
+        [SerializeField] float _maxLifeTime = 10f;
+        
+        VerticalMover _mover;
+        float _currentLifeTime = 0f;
+
+        public float MoveSpeed => _moveSpeed;
+
+        void Awake()
+        {
+            _mover = new VerticalMover(this);
+        }
+
+        void Update()
+        {
+            _currentLifeTime += Time.deltaTime;
+
+            if (_currentLifeTime > _maxLifeTime)
+            {
+                _currentLifeTime = 0f;
+                KillYourself();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            _mover.FixedTick();
+        }
+        
+        void KillYourself()
+        {
+            Destroy(this.gameObject);
+        }
+    }    
+}
+
---
+++ UdemyProject2/Assets/GameFolders/Scripts/Concretes/Movements/VerticalMover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UdemyProject2.Controllers;
+using UnityEngine;
+
+namespace UdemyProject2.Movements
+{
+    public class VerticalMover
+    {
+        EnemyController _enemyController;
+        float _moveSpeed;
+        
+        public VerticalMover(EnemyController enemyController)
+        {
+            _enemyController = enemyController;
+            _moveSpeed = _enemyController.MoveSpeed;
+        }
+
+        public void FixedTick(float vertical = 1)
+        {
+            _enemyController.transform.Translate(Vector3.back * vertical * _moveSpeed * Time.deltaTime);
+        }
+    }    
+}
+
