9b41b1bf9bf0058bb1057cef3d3fb3b998554d55
berkterek <42406263+berkterek@users.noreply.github.com>
2021-02-22 16:43:43 +0300

    EnemySpanwer Script
    EnemySpanwer script tamamladi ve random sureler boyunca enemy spawn'laniyor

---
+++ UdemyProject2/Assets/GameFolders/Scripts/Concretes/Controllers/SpawnerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyProject2.Controllers
+{
+    public class SpawnerController : MonoBehaviour
+    {
+        [SerializeField] EnemyController _enemyPrefab;
+        [Range(0.1f, 5f)] [SerializeField] float _min = 0.1f;
+        [Range(6f, 15f)] [SerializeField] float _max = 15f;
+        
+        float _maxSpawnTime;
+        float _currentSpawnTime = 0f;
+
+        void OnEnable()
+        {
+            GetRandomMaxTime();
+        }
+
+        void Update()
+        {
+            _currentSpawnTime += Time.deltaTime;
+
+            if (_currentSpawnTime > _maxSpawnTime)
+            {
+                Spawn();
+            }
+        }
+
+        void Spawn()
+        {
+            EnemyController newEnemy = Instantiate(_enemyPrefab, transform.position, transform.rotation);
+            newEnemy.transform.parent = this.transform;
+
+            _currentSpawnTime = 0f;
+            GetRandomMaxTime();
+        }
+
+        private void GetRandomMaxTime()
+        {
+            _maxSpawnTime = Random.Range(_min, _max);
+        }
+    }
+}
