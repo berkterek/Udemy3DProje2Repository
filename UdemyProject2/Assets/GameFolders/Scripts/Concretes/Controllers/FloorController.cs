60b440c5f98efbb0ceaf8f97af9b849727a33591
berkterek <42406263+berkterek@users.noreply.github.com>
2021-02-22 15:25:13 +0300

    Floor Script
    Floor script Floor Prefab'ine verildi oda hepsini variant'larina dagitti simdilik bir tane varainat var

---
+++ UdemyProject2/Assets/GameFolders/Scripts/Concretes/Controllers/FloorController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyProject2.Controllers
+{
+    public class FloorController : MonoBehaviour
+    {
+        [Range(0.5f,2f)]
+        [SerializeField] float _moveSpeed = 5f;
+        Material _material;
+
+        void Awake()
+        {
+            _material = GetComponentInChildren<MeshRenderer>().material;
+        }
+
+        void Update()
+        {
+            _material.mainTextureOffset += Vector2.down * Time.deltaTime * _moveSpeed;
+        }
+    }    
+}
+
