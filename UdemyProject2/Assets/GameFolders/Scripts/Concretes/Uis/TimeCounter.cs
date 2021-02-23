6c986db2cf842f34312c074b39611c2991e4bb4f
berkterek <42406263+berkterek@users.noreply.github.com>
2021-02-23 11:54:48 +0300

    TimeCounter
    TimeCounter script yazildi ve oyuncunun yanmadan butun oıyun suresini gosteren scripttir

---
+++ UdemyProject2/Assets/GameFolders/Scripts/Concretes/Uis/TimeCounter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace UdemyProject2.Uis
+{
+    public class TimeCounter : MonoBehaviour
+    {
+        TMP_Text _text;
+        float _currentTime;
+
+        void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        void Update()
+        {
+            _currentTime += Time.deltaTime;
+            _text.text = _currentTime.ToString("0");
+        }
+    }    
+}
+
